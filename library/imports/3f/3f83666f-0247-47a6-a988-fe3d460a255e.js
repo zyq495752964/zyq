1add86a2b9f2b2dd3e8ff2ff438eab18ba94325f
495752964@qq.com <495752964@qq.com>
2018-04-27 11:05:29 +0800

    https://github.com/zyq495752964/zyq.git

---
+++ library/imports/3f/3f83666f-0247-47a6-a988-fe3d460a255e.js
@@ -0,0 +1,126 @@
+"use strict";
+cc._RF.push(module, '3f836ZvAkdHpqmI/j1GCiVe', 'Game');
+// scripts/Game.js
+
+'use strict';
+
+var _cc$Class;
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+// Learn cc.Class:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/class.html
+// Learn Attribute:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/creator/en/scripting/life-cycle-callbacks.html
+
+cc.Class((_cc$Class = {
+    extends: cc.Component,
+    // Game.js
+    properties: {
+        // 这个属性引用了星星预制资源
+        starPrefab: {
+            default: null,
+            type: cc.Prefab
+        },
+        // 星星产生后消失时间的随机范围
+        maxStarDuration: 0,
+        minStarDuration: 0,
+        // 地面节点，用于确定星星生成的高度
+        ground: {
+            default: null,
+            type: cc.Node
+        },
+        // player 节点，用于获取主角弹跳的高度，和控制主角行动开关
+        player: {
+            default: null,
+            type: cc.Node
+        },
+        scoreDisplay: {
+            default: null,
+            type: cc.Label
+        },
+        scoreAudio: {
+            default: null,
+            url: cc.AudioClip
+        }
+    },
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {},
+    // Game.js
+    onLoad: function onLoad() {
+        // 获取地平面的 y 轴坐标
+        this.groundY = this.ground.y + this.ground.height / 2;
+        // 生成一个新的星星
+        this.timer = 0;
+        this.starDuration = 0;
+        // 生成一个新的星星
+        this.spawnNewStar();
+        // 初始化计分
+        this.score = 0;
+    },
+
+    spawnNewStar: function spawnNewStar() {
+        // 使用给定的模板在场景中生成一个新节点
+        var newStar = cc.instantiate(this.starPrefab);
+        // 将新增的节点添加到 Canvas 节点下面
+        this.node.addChild(newStar);
+        // 为星星设置一个随机位置
+        newStar.setPosition(this.getNewStarPosition());
+        newStar.getComponent('Star').game = this;
+        this.starDuration = this.minStarDuration + cc.random0To1() * (this.maxStarDuration - this.minStarDuration);
+        this.timer = 0;
+    },
+
+    getNewStarPosition: function getNewStarPosition() {
+        var randX = 0;
+        // 根据地平面位置和主角跳跃高度，随机得到一个星星的 y 坐标
+        var randY = this.groundY + cc.random0To1() * this.player.getComponent('Player').jumpHeight + 50;
+        // 根据屏幕宽度，随机得到一个星星 x 坐标
+        var maxX = this.node.width / 2;
+        randX = cc.randomMinus1To1() * maxX;
+        // 返回星星坐标
+        return cc.p(randX, randY);
+    },
+
+    start: function start() {},
+
+
+    // update (dt) {},
+    // Game.js
+    update: function update(dt) {
+        // 每帧更新计时器，超过限度还没有生成新的星星
+        // 就会调用游戏失败逻辑
+        if (this.timer > this.starDuration) {
+            this.gameOver();
+            return;
+        }
+        this.timer += dt;
+    },
+    gainScore: function gainScore() {
+        this.score += 1;
+        // 更新 scoreDisplay Label 的文字
+        this.scoreDisplay.string = 'Score: ' + this.score.toString();
+        cc.audioEngine.playEffect(this.scoreAudio, false);
+    }
+}, _defineProperty(_cc$Class, 'gainScore', function gainScore() {
+    this.score += 1;
+    // 更新 scoreDisplay Label 的文字
+    this.scoreDisplay.string = 'Score: ' + this.score.toString();
+    this.score += 1;
+    // 更新 scoreDisplay Label 的文字
+    this.scoreDisplay.string = 'Score: ' + this.score.toString();
+    // 播放得分音效
+    cc.audioEngine.playEffect(this.scoreAudio, false);
+}), _defineProperty(_cc$Class, 'gameOver', function gameOver() {
+    this.player.stopAllActions(); //停止 player 节点的跳跃动作
+    cc.director.loadScene('game');
+}), _cc$Class));
+
+cc._RF.pop();
